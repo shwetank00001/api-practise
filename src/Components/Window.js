ce02e21425440b8e2851dd03502cb675cc619ee3
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2023-04-16 20:44:50 +0530

    all done

--- src/Components/Window.js
+++ src/Components/Window.js
@@ -1,12 +1,18 @@
 import React from 'react'
 
 const Window = () => {
-    
-    React.useEffect
+
+    const [windowWid, setWindowWidth] = React.useState(window.innerWidth)
+
+    React.useEffect(function(){
+        window.addEventListener("resize", function(){
+            setWindowWidth(window.innerWidth)
+            console.log("resize done")
+        })
+    },[])
   return (
     <div>
-        <h1>
-            {window.innerWidth}
+        <h1> The width is : {windowWid}
         </h1>
     </div>
   )
